87d26571b4c7aff3e9810cd945e298a1758272a5
AldenBoby <boby.alden128@gmail.com>
2020-02-24 23:52:44 +0200

    added test for setdelivered

--- FlowerShopTests/UnitTests.cs
+++ FlowerShopTests/UnitTests.cs
@@ -1,4 +1,6 @@
 using NUnit.Framework;
+using FlowerShop;
+using NSubstitute;
 
 namespace Tests
 {
@@ -11,8 +13,15 @@ namespace Tests
 
         [Test]
         public void Test1()
-        {
-            Assert.Pass();
+        {   
+            //ARRANGE
+            IClient c = Substitute.For<IClient>();
+            IOrderDAO b = Substitute.For<IOrderDAO>();
+            Order a = Substitute.For<Order>(b,c);
+            //ACT
+            a.Deliver();
+            //ASSERT
+            b.Received().SetDelivered(a);
         }
     }
 }
